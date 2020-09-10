c312ba874abb6478c066762ff75a93224c942169
Will Meister <william.k.meister@gmail.com>
2020-09-10 17:39:04 -0500

    Getting context for github actions script

--- .github/scripts/replace-aws-account-id.sh
+++ .github/scripts/replace-aws-account-id.sh
@@ -3,7 +3,6 @@
 echo "Current dir: $(pwd)"
 echo "Dir name: $(dirname $0)"
 echo "Dir contents: \n $(ls -alh)"
-echo "docker compose: $(ls -alh ../../docker-compose.ci.yml)"
 
-sed -i '' -E 's/<AWS_CI_AWS_ACCOUNT_ID>/'"$AWS_ACCOUNT_ID"'/g' ../../docker-compose.ci.yml
+sed -i '' -E 's/<AWS_CI_AWS_ACCOUNT_ID>/'"$AWS_ACCOUNT_ID"'/g' ./docker-compose.ci.yml
 
