cbf6f375885a2ea63946278341c15f0ceb38875f
Mark Tyneway <mark.tyneway@gmail.com>
2020-09-22 21:00:12 -0700

    tests: add script helpers, test chainid return value (#4)

--- packages/integration-tests/test/rpc.spec.ts
+++ packages/integration-tests/test/rpc.spec.ts
@@ -23,7 +23,7 @@ const mnemonic =
 // Address derived at m/44'/60'/0'/0 of test mnemonic
 const etherbase = '0x9858EfFD232B4033E47d90003D41EC34EcaEda94'
 
-describe('sendTransaction', () => {
+describe('Transactions', () => {
   let provider
 
   before(async () => {
@@ -132,5 +132,11 @@ describe('sendTransaction', () => {
     for (const estimate of estimates) {
       estimate.toNumber().should.eq(7999999)
     }
+  })
+
+  it('should get correct chainid', async () => {
+    const chainId = await provider.send('eth_chainId', [])
+    chainId.should.eq('0x1a4')
+    parseInt(chainId, 16).should.eq(420)
   })
 })
