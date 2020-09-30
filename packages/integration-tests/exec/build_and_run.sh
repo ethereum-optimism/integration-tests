db615c2b67db5ee71915964bdf67b4c126de83c6
Mark Tyneway <mark.tyneway@gmail.com>
2020-09-30 08:17:55 -0700

    integration monorepo (#7)
    * testing: update to work with monorepo
    
    * docker: build or run

--- packages/integration-tests/exec/build_and_run.sh
+++ packages/integration-tests/exec/build_and_run.sh
@@ -7,22 +7,17 @@ set -e
 
 cmd=$@
 
-ROOT_DIR=../../..
-
-
 if [ -n "$REBUILD" ]; then
-  echo -e "\n\nREBUILD env var set, rebuilding...\n\n"
+  echo -e "\nRebuilding integration tests\n"
 
   if [ -n "$FETCH_DEPS" ]; then
     echo -e "\nFetching dependencies (this will take forever the first time time)..."
-    yarn --cwd $ROOT_DIR --verbose --frozen-lockfile
+    yarn --frozen-lockfile --verbose
   fi
 
-  yarn --cwd $ROOT_DIR clean
-  yarn --cwd $ROOT_DIR build
-  echo -e "\n\nCode built proceeding with ./wait_for_dependencies.sh...\n\n"
+  yarn clean
+  yarn build
 else
-  echo -e "\n\nREBUILD env var not set, calling ./wait_for_dependencies.sh without building...\n\n"
+  echo "Starting the integration tests"
+  exec $(dirname $0)/wait_for_dependencies.sh "$cmd"
 fi
-
-exec $(dirname $0)/wait_for_dependencies.sh "$cmd"
