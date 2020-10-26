fc95b557ea3278a20b5a0427a961f7f127378f4e
Mark Tyneway <mark.tyneway@gmail.com>
2020-10-26 10:10:37 -0700

    overhaul: for new architecture

--- exec/build_and_run.sh
+++ exec/build_and_run.sh
@@ -7,17 +7,5 @@ set -e
 
 cmd=$@
 
-if [ -n "$REBUILD" ]; then
-  echo -e "\nRebuilding integration tests\n"
-
-  if [ -n "$FETCH_DEPS" ]; then
-    echo -e "\nFetching dependencies (this will take forever the first time time)..."
-    yarn --frozen-lockfile
-  fi
-
-  yarn clean
-  yarn build
-else
-  echo "Starting the integration tests"
-  exec $(dirname $0)/wait_for_dependencies.sh "$cmd"
-fi
+echo "Starting the integration tests"
+exec $(dirname $0)/wait_for_dependencies.sh "$cmd"
--- exec/wait_for_dependencies.sh
+++ exec/wait_for_dependencies.sh
@@ -5,22 +5,6 @@ set -e
 cmd="$@"
 
 RETRIES=30
-
-echo "Connection info: ${POSTGRES_HOST}:${POSTGRES_PORT}, user=${POSTGRES_USER}, postgres db=${POSTGRES_DATABASE}"
-
-until PGPASSWORD=$POSTGRES_PASSWORD psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER -d $POSTGRES_DATABASE -c "select 1" > /dev/null 2>&1 || [ $RETRIES -eq 0 ]; do
-  echo "Waiting for Postgres server, $((RETRIES--)) remaining attempts..."
-  sleep 1
-done
-
-if [ $RETRIES -eq 0 ]; then
-  echo "Timeout reached waiting for Postgres!"
-  exit 1
-fi
-
-echo "Connected to Postgres"
-
-RETRIES=30
 echo "Connecting to L2 Geth..."
 until $(curl --output /dev/null --silent --fail -H "Content-Type: application/json" -d '{"jsonrpc": "2.0", "id": 9999999, "method": "net_version"}' $L2_NODE_WEB3_URL); do
   sleep 1
