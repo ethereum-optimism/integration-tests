daf9e6396a00ef1802aa4f22adb1f04263499b4f
Mark Tyneway <mark.tyneway@gmail.com>
2020-11-05 08:24:50 -0800

    WIP: sequencer batch append tests

--- packages/tx-ingestion/test/sequencer-batch-append.spec.ts
+++ packages/tx-ingestion/test/sequencer-batch-append.spec.ts
@@ -4,13 +4,13 @@
  * https://github.com/ethereum-optimism
  */
 
-import { Config, etherbase } from '../../../common'
+import { Config, etherbase, sleep } from '../../../common'
 import { Web3Provider, JsonRpcProvider } from '@ethersproject/providers'
 import { ganache } from '@eth-optimism/ovm-toolchain'
 import { OptimismProvider } from '@eth-optimism/provider'
 import { deployContract } from 'ethereum-waffle'
-
-const ERC20 = require('../build/ERC20.json');
+import assert = require('assert')
+import ERC20 = require('../build/ERC20.json');
 
 describe.only('Queue Origin Sequencer Transactions', () => {
   let optimismProvider
@@ -29,9 +29,10 @@ describe.only('Queue Origin Sequencer Transactions', () => {
     provider = new JsonRpcProvider(Config.L2NodeUrlWithPort())
   })
 
+  const initalSupply = 1000
   before(async () => {
     signer = await provider.getSigner()
-    token = await deployContract(signer, ERC20, [1000, 'Foo', 8, 'FOO'])
+    token = await deployContract(signer, ERC20, [initalSupply, 'Foo', 8, 'FOO'])
   })
 
   it('should send a transaction', async () => {
@@ -39,14 +40,16 @@ describe.only('Queue Origin Sequencer Transactions', () => {
     const address = await signer.getAddress()
     const nonce = await provider.getTransactionCount(address)
 
-    const result = await token.transfer(etherbase, 100000, {
-      nonce,
-      gasLimit: 41004,
-      gasPrice: 0,
-    })
-
-    console.log(result)
+    const result = await token.transfer(etherbase, 1)
     const receipt = await result.wait()
-    console.log(receipt)
+    assert(receipt)
+  })
+
+  it('should batch submit', async () => {
+    do {
+      const tip = await provider.getBlock('latest')
+      console.log(tip)
+      await sleep(5000)
+    } while (true)
   })
 })
