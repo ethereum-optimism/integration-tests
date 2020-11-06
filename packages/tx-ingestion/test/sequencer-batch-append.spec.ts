5a3438b8de071a853f53b56c11392c7a204e4639
Mark Tyneway <mark.tyneway@gmail.com>
2020-11-06 09:30:38 -0800

    disable sequencer batch append test

--- packages/tx-ingestion/test/sequencer-batch-append.spec.ts
+++ packages/tx-ingestion/test/sequencer-batch-append.spec.ts
@@ -12,7 +12,9 @@ import { deployContract } from 'ethereum-waffle'
 import assert = require('assert')
 import ERC20 = require('../build/ERC20.json');
 
-describe.only('Queue Origin Sequencer Transactions', () => {
+// TODO(mark): Remove the skip of this test when
+// the verifier is enabled in geth
+describe.skip('Queue Origin Sequencer Transactions', () => {
   let optimismProvider
   let provider: JsonRpcProvider
   let token
@@ -31,11 +33,22 @@ describe.only('Queue Origin Sequencer Transactions', () => {
 
   const initalSupply = 1000
   before(async () => {
+    const pre = await provider.getBlock('latest')
+
     signer = await provider.getSigner()
     token = await deployContract(signer, ERC20, [initalSupply, 'Foo', 8, 'FOO'])
+
+    // Allow the batch to be submitted
+    do {
+      const tip = await provider.getBlock('latest')
+      await sleep(5000)
+      if (tip.number === pre.number + 1) {
+        break
+      }
+    } while (true)
   })
 
-  it('should send a transaction', async () => {
+  it('should sequencer batch append', async () => {
     const chainId = await signer.getChainId()
     const address = await signer.getAddress()
     const nonce = await provider.getTransactionCount(address)
@@ -43,13 +56,5 @@ describe.only('Queue Origin Sequencer Transactions', () => {
     const result = await token.transfer(etherbase, 1)
     const receipt = await result.wait()
     assert(receipt)
-  })
-
-  it('should batch submit', async () => {
-    do {
-      const tip = await provider.getBlock('latest')
-      console.log(tip)
-      await sleep(5000)
-    } while (true)
   })
 })
