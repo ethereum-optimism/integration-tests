7ab2307b00fd77aeef4b6d29c0a1484cac7a9e13
Alexander <alexanderattar@gmail.com>
2020-12-08 14:54:09 -0500

    More fixes based on feedback

--- packages/integration-tests/test/actions.spec.ts
+++ packages/integration-tests/test/actions.spec.ts
@@ -1,4 +1,4 @@
-import { Config } from '../../../common'
+import { Config, sleep } from '../../../common'
 import { Web3Provider } from '@ethersproject/providers'
 import { ganache } from '@eth-optimism/ovm-toolchain'
 import assert = require('assert')
@@ -11,10 +11,7 @@ const {
 } = require('ethers');
 const fs = require('fs')
 
-const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
-
 let SimpleStorage
-let L2Messenger
 const L1_USER_PRIVATE_KEY = Config.L1UserPrivateKey()
 const L2_USER_PRIVATE_KEY = Config.L2UserPrivateKey()
 const goerliURL = Config.L1NodeUrlWithPort()
@@ -29,14 +26,14 @@ const l2MessengerJSON = JSON.parse(fs.readFileSync('contracts/build/OVM_L2CrossD
 const deploySimpleStorage = async () => {
   const SimpleStorageJson = JSON.parse(fs.readFileSync('contracts/build/SimpleStorage.json'))
   const SimpleStorageFactory = new ContractFactory(SimpleStorageJson.abi, SimpleStorageJson.bytecode, l2Wallet)
-  SimpleStorage = await SimpleStorageFactory.deploy()
+  return SimpleStorageFactory.deploy()
 }
 
-const deposit = async (amount, ...args) => {
+const deposit = async (amount, value) => {
   const L1Messenger = new Contract(Config.L1MessengerAddress(), messengerJSON.abi, l1Wallet)
-  L2Messenger = new Contract(Config.L2MessengerAddress(), l2MessengerJSON.abi, l2Wallet)
+  const L2Messenger = new Contract(Config.L2MessengerAddress(), l2MessengerJSON.abi, l2Wallet)
 
-  const calldata = SimpleStorage.interface.encodeFunctionData('setValue', [`0x${'42'.repeat(32)}`])
+  const calldata = SimpleStorage.interface.encodeFunctionData('setValue', [value])
   const l1ToL2Tx = await L1Messenger.sendMessage(
     SimpleStorage.address,
     calldata,
@@ -62,11 +59,12 @@ describe('Messages', async () => {
   })
 
   it('should deploy the simple storage contract', async () => {
-    await deploySimpleStorage()
+    SimpleStorage = await deploySimpleStorage()
   })
 
   it('should deposit from L1->L2', async () => {
-    await deposit(1)
+    const value = `0x${'42'.repeat(32)}`
+    await deposit(1, value)
     const msgSender = await SimpleStorage.msgSender()
     const l1ToL2Sender = await SimpleStorage.l1ToL2Sender()
     const storageVal = await SimpleStorage.value()
@@ -74,7 +72,7 @@ describe('Messages', async () => {
 
     msgSender.should.be.eq('0x4200000000000000000000000000000000000007')
     l1ToL2Sender.should.be.eq('0x023fFdC1530468eb8c8EEbC3e38380b5bc19Cc5d')
-    storageVal.should.be.eq('0x4242424242424242424242424242424242424242424242424242424242424242')
+    storageVal.should.be.eq(`0x${'42'.repeat(32)}`)
     count.toNumber().should.be.eq(1)
   })
 })
