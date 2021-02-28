bae218e70cd56571afea3ef44016589a5a373727
Mark Tyneway <mark.tyneway@gmail.com>
2021-02-27 18:00:35 -0800

    fix: better error messages (#49)
    * fix: better error messages
    
    * fix: better error messages for all
    
    * fix: remove particular messages

--- common.ts
+++ common.ts
@@ -36,10 +36,16 @@ export const getl2Provider = (): Provider => {
 
 export class Config {
   public static L1NodeUrlWithPort(): string {
+    if (process.env.L1_NODE_WEB3_URL === undefined) {
+      throw new Error('L1_NODE_WEB3_URL is undefined')
+    }
     return process.env.L1_NODE_WEB3_URL
   }
 
   public static L2NodeUrlWithPort(): string {
+    if (process.env.L1_NODE_WEB3_URL === undefined) {
+      throw new Error('L2_NODE_WEB3_URL is undefined')
+    }
     return process.env.L2_NODE_WEB3_URL
   }
 
@@ -50,10 +56,16 @@ export class Config {
   /* Contracts */
   // TODO: this is the address manager
   public static AddressResolverAddress(): string {
+    if (process.env.ETH1_ADDRESS_RESOLVER_ADDRESS === undefined) {
+      throw new Error('ETH1_ADDRESS_RESOLVER_ADDRESS is undefined')
+    }
     return process.env.ETH1_ADDRESS_RESOLVER_ADDRESS
   }
 
   public static SequencerPrivateKey(): string {
+    if (process.env.SEQUENCER_PRIVATE_KEY === undefined) {
+      throw new Error('SEQUENCER_PRIVATE_KEY is undefined')
+    }
     return process.env.SEQUENCER_PRIVATE_KEY
   }
 
@@ -62,6 +74,9 @@ export class Config {
   }
 
   public static DeployerPrivateKey(): string {
+    if (process.env.DEPLOYER_PRIVATE_KEY === undefined) {
+      throw new Error('DEPLOYER_PRIVATE_KEY is undefined')
+    }
     return process.env.DEPLOYER_PRIVATE_KEY
   }
 
