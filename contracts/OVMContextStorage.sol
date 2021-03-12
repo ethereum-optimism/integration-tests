1b8045926db9f9b7a8d6a1383bef6836f4ac337e
Mark Tyneway <mark.tyneway@gmail.com>
2021-03-12 10:10:54 -0800

    refactor: include timestamp

--- contracts/OVMContextStorage.sol
+++ contracts/OVMContextStorage.sol
@@ -1,11 +1,15 @@
+// SPDX-License-Identifier: MIT
+
 pragma solidity >=0.7.0;
 
-contract BlockNumberStorage {
+contract OVMContextStorage {
     mapping (uint256 => uint256) public blockNumbers;
+    mapping (uint256 => uint256) public timestamps;
     uint256 public index = 0;
 
     fallback() external {
         blockNumbers[index] = block.number;
+        timestamps[index] = block.timestamp;
         index++;
     }
 }
