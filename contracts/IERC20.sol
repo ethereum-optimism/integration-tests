6f5e51ae3eef3539453d70195b477c22a5873586
Mark Tyneway <mark.tyneway@gmail.com>
2021-03-19 16:34:04 -0700

    blocknumber: fix (#59)
    * blocknumber: fix
    
    * refactor: include timestamp
    
    * fix: remove debug info
    
    * contracts: make compile
    
    * tests: address nits

--- contracts/IERC20.sol
+++ contracts/IERC20.sol
@@ -1,9 +1,11 @@
+// SPDX-License-Identifier: MIT
+
 // Abstract contract for the full ERC 20 Token standard
 // https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20.md
-pragma solidity ^0.5.16;
+pragma solidity >=0.7.0 <0.8.0;
 
 
-contract IERC20 {
+interface IERC20 {
     /* This is a slight change to the ERC20 base standard.
     function totalSupply() constant returns (uint256 supply);
     is replaced with:
@@ -14,34 +16,34 @@ contract IERC20 {
     function by the compiler.
     */
     /// total amount of tokens
-      function totalSupply() external view returns (uint256);
+    function totalSupply() external view returns (uint256);
 
     /// @param _owner The address from which the balance will be retrieved
-    /// @return The balance
+    /// @return balance The balance
     function balanceOf(address _owner) external view returns (uint256 balance);
 
     /// @notice send `_value` token to `_to` from `msg.sender`
     /// @param _to The address of the recipient
     /// @param _value The amount of token to be transferred
-    /// @return Whether the transfer was successful or not
+    /// @return success Whether the transfer was successful or not
     function transfer(address _to, uint256 _value) external returns (bool success);
 
     /// @notice send `_value` token to `_to` from `_from` on the condition it is approved by `_from`
     /// @param _from The address of the sender
     /// @param _to The address of the recipient
     /// @param _value The amount of token to be transferred
-    /// @return Whether the transfer was successful or not
+    /// @return success Whether the transfer was successful or not
     function transferFrom(address _from, address _to, uint256 _value) external returns (bool success);
 
     /// @notice `msg.sender` approves `_spender` to spend `_value` tokens
     /// @param _spender The address of the account able to transfer the tokens
     /// @param _value The amount of tokens to be approved for transfer
-    /// @return Whether the approval was successful or not
+    /// @return success Whether the approval was successful or not
     function approve(address _spender, uint256 _value) external returns (bool success);
 
     /// @param _owner The address of the account owning tokens
     /// @param _spender The address of the account able to transfer the tokens
-    /// @return Amount of remaining tokens allowed to spent
+    /// @return remaining  Amount of remaining tokens allowed to spent
     function allowance(address _owner, address _spender) external view returns (uint256 remaining);
 
     // solhint-disable-next-line no-simple-event-func-name
