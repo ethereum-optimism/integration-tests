4e150877a74ccdec6fb1aa237397a653383162e2
Kelvin Fichter <kelvinfichter@gmail.com>
2021-03-19 16:14:56 -0700

    Clean up test and fix implementation

--- packages/sequencer-interactions/test/read-proxy-event.spec.ts
+++ packages/sequencer-interactions/test/read-proxy-event.spec.ts
@@ -1,70 +1,70 @@
 import { expect } from 'chai'
+
+/* Imports: External */
 import { JsonRpcProvider } from '@ethersproject/providers'
 import { Contract, ContractFactory, Wallet } from 'ethers'
-import { Config } from '../../../common'
-import erc20Json = require('../../../contracts/build-ovm/ERC20.json')
-import upgradeableProxyJson = require('../../../contracts/build-ovm/UpgradeableProxy.json')
 
-describe('Read proxy event', () => {
+/* Imports: Internal */
+import { Config } from '../../../common'
+import ERC20ABI = require('../../../contracts/build-ovm/ERC20.json')
+import UpgradeableProxyABI = require('../../../contracts/build-ovm/UpgradeableProxy.json')
+
+describe('Reading events from proxy contracts', () => {
   let l2Provider: JsonRpcProvider
   let l2Wallet: Wallet
-
   before(async () => {
-    // l1Provider = new JsonRpcProvider(Config.L1NodeUrlWithPort())
-    // l1Signer = new Wallet(Config.DeployerPrivateKey()).connect(l1Provider))
-    const key =
-    '0xa35617f4fe630bf50024fcbe2c051d2dffe5ea19695b2d660ce4db7a5acdcc30'
     l2Provider = new JsonRpcProvider(Config.L2NodeUrlWithPort())
-    l2Wallet = new Wallet(key).connect(l2Provider)
-    // l2Wallet = await l2Provider.getSigner()
+    l2Wallet = new Wallet(Config.DeployerPrivateKey()).connect(l2Provider)
   })
 
-  it('should read the event correctly', async () => {
-    const initialAmount = 1000
-    const tokenName = 'OVM Test'
-    const tokenDecimals = 8
-    const TokenSymbol = 'TOK'
-
-    const implFactory = new ContractFactory(
-      erc20Json.abi,
-      erc20Json.bytecode,
+  it('should read transfer events from a proxy ERC20', async () => {
+    // Set up our contract factories in advance.
+    const Factory__ERC20 = new ContractFactory(
+      ERC20ABI.abi,
+      ERC20ABI.bytecode,
       l2Wallet
     )
-    const impl = await implFactory.deploy(initialAmount, tokenName, tokenDecimals, TokenSymbol)
-    await impl.deployTransaction.wait()
-
-    // erc20Json.abi[0].inputs = []
-    const proxyFactory = new ContractFactory(
-      upgradeableProxyJson.abi,
-      // [...erc20Json.abi, ...upgradeableProxyJson.abi],
-      upgradeableProxyJson.bytecode,
+    const Factory__UpgradeableProxy = new ContractFactory(
+      [...ERC20ABI.abi, ...UpgradeableProxyABI.abi],
+      UpgradeableProxyABI.bytecode,
       l2Wallet
     )
-    const proxy = await proxyFactory.deploy(impl.address, Buffer.from(''))
-    await proxy.deployTransaction.wait()
 
-    const contract = new Contract(proxy.address, erc20Json.abi, l2Wallet)
+    // Deploy the underlying ERC20 implementation.
+    const ERC20 = await Factory__ERC20.deploy()
+    await ERC20.deployTransaction.wait()
 
-    const _transfer = async (to, amount) => {
-      const transferTx = await contract.transfer(to, amount);
-      await transferTx.wait();
-    };
+    // Deploy the upgradeable proxy and execute the init function.
+    const ProxyERC20 = await Factory__UpgradeableProxy.deploy(
+      ERC20.address,
+      ERC20.interface.encodeFunctionData('init', [
+        1000, // initial supply
+        'Cool Token Name Goes Here', // token name
+      ])
+    )
+    await ProxyERC20.deployTransaction.wait()
 
-    // Make 2x Transfer
-    const recipient = '0x0000000000000000000000000000000000000000';
-    await _transfer(recipient, '1');
-    await _transfer(recipient, '1');
+    // Make two transfers.
+    const recipient = '0x0000000000000000000000000000000000000000'
+    const transfer1 = await ProxyERC20.transfer(recipient, 1)
+    await transfer1.wait()
+    const transfer2 = await ProxyERC20.transfer(recipient, 1)
+    await transfer2.wait()
 
-    const _queryFilterTransfer = async (queryContract, filterContract) => {
+    const _queryFilterTransfer = async (
+      queryContract: Contract,
+      filterContract: Contract
+    ) => {
       // Get the filter
-      const filter = filterContract.filters.Transfer(null, null, null);
+      const filter = filterContract.filters.Transfer(null, null, null)
       // Query the filter
-      return queryContract.queryFilter(filter, 0, 'latest');
-    };
+      return queryContract.queryFilter(filter, 0, 'latest')
+    }
 
-    expect((await _queryFilterTransfer(impl, impl)).length).to.eq(0)
-    expect((await _queryFilterTransfer(impl, contract)).length).to.eq(0)
-    expect((await _queryFilterTransfer(contract, impl)).length).to.eq(2)
-    expect((await _queryFilterTransfer(contract, contract)).length).to.eq(2)
+    // Make sure events are being emitted in the right places.
+    expect((await _queryFilterTransfer(ERC20, ERC20)).length).to.eq(0)
+    expect((await _queryFilterTransfer(ERC20, ProxyERC20)).length).to.eq(0)
+    expect((await _queryFilterTransfer(ProxyERC20, ERC20)).length).to.eq(2)
+    expect((await _queryFilterTransfer(ProxyERC20, ProxyERC20)).length).to.eq(2)
   })
 })
