630fb6a6f68bfc4193e17bf7ca68dbc5d33daf6a
Kelvin Fichter <kelvinfichter@gmail.com>
2021-03-25 14:39:42 -0700

    Minor tweaks to test.sh

--- packages/test-synthetix/test.sh
+++ packages/test-synthetix/test.sh
@@ -1,6 +1,6 @@
-git clone git@github.com:Synthetixio/synthetix.git
+#!/bin/bash
+git clone --depth=1 --branch develop https://github.com/Synthetixio/synthetix.git
 cd synthetix
-git checkout develop
 npm install
 node publish deploy-ovm-pair
 npm run test:prod:ovm
